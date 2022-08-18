761584527204679ead4fc5481a69348b151da5c2
LeeSin <endlesspeak@163.com>
2022-08-18 14:12:30 +0800

    setting up config files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,42 @@
+# alias emacs='emacs -nw'
+alias epa='export http_proxy=http://127.0.0.1:20171 && https_proxy=http://127.0.0.1:20171'
+alias epq='export https_proxy="127.0.0.1:8889" && http_proxy="127.0.0.1:8889"'
+alias uep='unset https_proxy && unset http_proxy'
+# alias av='source venv/bin/activate'
+alias c='clear'
+alias cdiff='colordiff'
+# alias cs='calcurse'
+alias dv='deactivate'
+# alias gc='git config credential.helper store'
+alias gg='git clone'
+alias ipy='ipython'
+# alias l='ls -la'
+alias lg='lazygit'
+# alias ms='mailsync'
+# alias mt='neomutt'
+alias r='echo $RANGER_LEVEL'
+# alias pu='python3 -m pudb'
+alias ra='ranger'
+# ra() {
+	#if [ -z "$RANGER_LEVEL" ]
+	#then
+		#ranger
+	#else
+		#exit
+	#fi
+#}
+alias n='neofetch'
+# alias g='onefetch'
+alias sra='sudo -E ranger'
+alias sudo='sudo -E'
+# alias vim='nvim'
+# alias gs='git config credential.helper store'
+# alias bat='sudo tlp bat'
+# alias ac='sudo tlp ac'
+# alias gy='git-yolo'
+alias nb='newsboat -r'
+alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
+# alias ta='tmux a'
+# alias t='tmux'
+alias lo='lsof -p $(fps) +w'
+alias py="python"
---
+++ zsh/env.zsh
@@ -0,0 +1,12 @@
+export TERMINAL=alacritty
+export PATH=$HOME/.local/bin:$PATH
+export XDG_CONFIG_HOME=$HOME/.config
+#export TERM=xterm-256color
+export TERM=xterm-256color
+export TERM_ITALICS=true
+export RANGER_LOAD_DEFAULT_RC="false"
+#export TERM=screen-256color
+export EDITOR=emacs
+export ZSH_AUTOSUGGEST_USE_ASYNC=1
+export ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+export TERM=screen-256color-bce
